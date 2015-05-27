b8f5c821ab7b34971d65ef3c92bfc5dcc72812e8
bharath <bharathreddy_s@yahoo.com>
2015-05-27 22:59:52 +0530

    Initial Commit

---
+++ Eaxmple/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Eaxmple
+//
+//  Created by Bharath V on 27/05/15.
+//  Copyright (c) 2015 ProV. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
